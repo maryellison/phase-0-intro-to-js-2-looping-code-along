35e9dbec801544c3e1133e5f819387536709aad9
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-09-28 20:38:15 -0400

    commit tree 97785ebce4a6e671609292d8bb6b087289caa707

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const birthdayThankYou = []
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        birthdayThankYou.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+
+    return birthdayThankYou
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}   
+
+countDown (11);
